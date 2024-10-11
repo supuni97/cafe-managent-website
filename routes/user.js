c258d2e97d20eb9ac5b9f16e99bc6a88b5f45211
Supuni Sithara <sitharabandara1997@gmail.com>
2024-10-11 19:08:08 +0530

    Added login api with jwt token

--- routes/user.js
+++ routes/user.js
@@ -2,29 +2,59 @@ const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+//signup api
 router.post('/signup', (req, res) => {
     let user = req.body;
     query = "select email,password,status,role from user where email=?"
-    connection.query(query,[user.email],(err,results)=>{
-        if (!err){
-            if(results.length <= 0 ){
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0) {
                 query = "insert into user(name,contactNumber,email,password,status,role) values(?,?,?,?,'false','user')";
-                connection.query(query,[user.name,user.contactNumber,user.email,user.password],(err,results)=>{
-                    if(!err){
-                        return res.status(200).json({message:"Successfully registered"})
-                    } else{
+                connection.query(query, [user.name, user.contactNumber, user.email, user.password], (err, results) => {
+                    if (!err) {
+                        return res.status(200).json({ message: "Successfully registered" })
+                    } else {
                         return res.status(500).json(err);
                     }
                 })
-            } else{
-                return res.status(400).json({message:"Email already exists."});
+            } else {
+                return res.status(400).json({ message: "Email already exists." });
             }
-         }
+        }
         else {
             return res.status(500).json(err);
         }
     })
-   
+
+})
+
+//login api
+router.post('/login', (req, res) => {
+    const user = req.body;
+    query = "select email,password,status,role from user where email=?"
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0 || results[0].password != user.password) {
+                return res.status(401).json({ message: "Incorrect username or password." });
+            } else if (results[0].status === 'false') {
+                return res.status(401).json({ message: "Wait for admin approval" });
+            } else if (results[0].password == user.password) {
+                const response = { email: results[0].email, role: results[0].role }
+                const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8h' })
+                res.status(200).json({ token: accessToken });
+            } else {
+                return res.status(400).json({ message: "Something went wrong. Please try again." });
+
+            }
+        }
+        else {
+            return res.status(500).json(err);
+        }
+    })
+
 })
 
 module.exports = router;
