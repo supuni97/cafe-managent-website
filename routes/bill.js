d179d225230cc714040da5390146c00fdbfe8a36
Supuni Sithara <sitharabandara1997@gmail.com>
2024-10-17 18:37:09 +0530

    Implemented bill details retrive and delete apis

--- routes/bill.js
+++ routes/bill.js
@@ -7,6 +7,7 @@ let path = require('path');
 var fs = require('fs');
 var uuid = require('uuid');
 var auth = require('../services/authentication');
+const e = require('express');
 
 //generate report
 router.post('/generateReport', auth.authenticateToken, (req, res) => {
@@ -37,5 +38,65 @@ router.post('/generateReport', auth.authenticateToken, (req, res) => {
         }
     });
 });
+
+//get pdf api
+router.post('/getPdf', auth.authenticateToken, function(req, res) {
+    const orderDetails = req.body; 
+    const pdfPath = './generated_pdf/' + orderDetails.uuid + '.pdf';
+    
+    if (fs.existsSync(pdfPath)) {
+        res.contentType("application/pdf");
+        fs.createReadStream(pdfPath).pipe(res);
+    } else {
+        var productDetailsReport = JSON.parse(orderDetails.productDetails);
+        ejs.renderFile(path.join(__dirname, '', "report.ejs"), { productDetails: productDetailsReport, name: orderDetails.name, email: orderDetails.email, contactNumber: orderDetails.contactNumber, paymentMethod: orderDetails.paymentMethod, totalAmount: orderDetails.total }, (err, results) => {
+            if (err) {
+                return res.status(500).json(err);
+            } else {
+                pdf.create(results).toFile('./generated_pdf/' + orderDetails.uuid + ".pdf", function(err, data){
+                    if(err) {
+                        console.log(err);
+                        return res.status(500).json(err);
+                    } else {
+                        res.contentType("application/pdf");
+                        fs.createReadStream(pdfPath).pipe(res);
+                    }
+                });
+            }
+        });
+    }
+});
+
+//get all bills
+router.get('/getBills',auth.authenticateToken,(req,res,next)=>{
+    var query = "select * from bill order by id DESC";
+    connection.query(query,(err,results)=>{
+        if(!err){
+            return res.status(200).json(results);
+
+        }else{
+            return res.status(500).json(err);
+        }
+    })
+})
+
+//delete bills
+router.delete('/delete/:id',auth.authenticateToken,(req,res,next)=>{
+    const id = req.params.id;
+    var query = "delete from bill where id=?";
+
+    connection.query(query,[id],(err,results)=>{
+        if(!err){
+            if(results.affectedRows ==0){
+                return res.status(404).json({message:"Bill id not found"});
+            }else{
+                return res.status(200).json({message:"Bill deleted successfully"});
+
+            }
+        }else{
+            return res.status(500).json(err);
+        }
+    })
+})
 
 module.exports = router;
